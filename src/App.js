e7048ae674ed8a7d22d31755e8573c19c8924d1a
José Ben Hur <joseben.hur.86@gmail.com>
2022-01-30 18:55:24 -0300

    Commit

--- src/App.js
+++ src/App.js
@@ -1,37 +1,45 @@
 import React from 'react';
-import { Link, Switch, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Contato from './pages/Contato';
-import Sobre from './pages/Sobre';
+import './App.css';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Home from './Components/Home';
+import Login from './Components/Login/Login';
+import { UserStorage } from './UserContext';
+import User from './Components/User/User';
+import ProtectedRoute from './Components/Helper/ProtectedRoute';
+import Photo from './Components/Photo/Photo';
+import UserProfile from './Components/User/UserProfile';
+import NotFound from './Components/NotFound';
 
 function App() {
   return (
-    <>
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/contato">Contato</Link>
-        </li>
-        <li>
-          <Link to="/sobre">Sobre</Link>
-        </li>
-      </ul>
-    </nav>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/contato">
-          <Contato/>
-        </Route>
-        <Route exact path="/sobre">
-          <Sobre/>
-        </Route>
-      </Switch>
-    </>
+    <div className="App">
+      <HashRouter basename="/">
+        <UserStorage>
+          <Header />
+          <main className="AppBody">
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="login/*" element={<Login />} />
+              <Route
+                exact
+                path="conta/*"
+                element={
+                  <ProtectedRoute>
+                    <User />
+                  </ProtectedRoute>
+                }
+              />
+              <Route exact path="foto/:id" element={<Photo />} />
+              <Route exact path="perfil/:user" element={<UserProfile />} />
+              <Route exact path="*" element={<NotFound />} />
+            </Routes>
+          </main>
+          <Footer />
+        </UserStorage>
+      </HashRouter>
+    </div>
   );
 }
 
