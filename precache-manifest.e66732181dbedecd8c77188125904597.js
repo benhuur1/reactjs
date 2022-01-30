ebe146fba40d2674871a293aae0af9c51a6277e5
José Ben Hur <joseben.hur.86@gmail.com>
2022-01-30 18:01:15 -0300

    Updates

---
+++ precache-manifest.e66732181dbedecd8c77188125904597.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2d9d7557250228781663eaa5a5f0d55",
+    "url": "/reactjs/index.html"
+  },
+  {
+    "revision": "0cb1304b685020b2c61b",
+    "url": "/reactjs/static/js/2.fc358b7b.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/reactjs/static/js/2.fc358b7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04dc5557cb5e00f47419",
+    "url": "/reactjs/static/js/main.0cd7ab2c.chunk.js"
+  },
+  {
+    "revision": "07340a733f09118bbc8b",
+    "url": "/reactjs/static/js/runtime-main.afacd7e8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4e8eb28ff2f1353faec4d5fb1115ce21.js"
+  "/reactjs/precache-manifest.e66732181dbedecd8c77188125904597.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactjs/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
