86ac619eeaab8f19884398b00a343768ebc2be61
José Ben Hur <joseben.hur.86@gmail.com>
2022-01-30 15:14:38 -0300

    Updates

---
+++ precache-manifest.eb378d6e00104f0d1822dbf6f52cd26b.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d1121bc2682bb808b78310517939d02",
+    "url": "/reactjs/index.html"
+  },
+  {
+    "revision": "82e2acaf4e8ec563c8b9",
+    "url": "/reactjs/static/css/4.ba08dd29.chunk.css"
+  },
+  {
+    "revision": "2fcebf567da1e3b10dda",
+    "url": "/reactjs/static/css/main.9f82f8f7.chunk.css"
+  },
+  {
+    "revision": "f9ec09bc9c20dffc155d",
+    "url": "/reactjs/static/js/2.346bef16.chunk.js"
+  },
+  {
+    "revision": "3f5662159ef661f953bf",
+    "url": "/reactjs/static/js/3.984469fd.chunk.js"
+  },
+  {
+    "revision": "a90bb751b102ed9d8f9443a23ededd32",
+    "url": "/reactjs/static/js/3.984469fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82e2acaf4e8ec563c8b9",
+    "url": "/reactjs/static/js/4.2598dfba.chunk.js"
+  },
+  {
+    "revision": "2fcebf567da1e3b10dda",
+    "url": "/reactjs/static/js/main.18bd138d.chunk.js"
+  },
+  {
+    "revision": "e723106797c898306981",
+    "url": "/reactjs/static/js/runtime-main.94bffe78.js"
+  },
+  {
+    "revision": "4a6d6ceb26b2d64c10a2b26fa9c47aaf",
+    "url": "/reactjs/static/media/adicionar.4a6d6ceb.svg"
+  },
+  {
+    "revision": "8cc31290254329d8d3990af08d454cd7",
+    "url": "/reactjs/static/media/dogs-footer.8cc31290.svg"
+  },
+  {
+    "revision": "849a64433f3bdd08d38623156960aa56",
+    "url": "/reactjs/static/media/dogs.849a6443.svg"
+  },
+  {
+    "revision": "9f80ad3de4a46815a44aa1a78bfc37dd",
+    "url": "/reactjs/static/media/enviar.9f80ad3d.svg"
+  },
+  {
+    "revision": "5b48807f51a9c4c87f8a5f802cf35aa0",
+    "url": "/reactjs/static/media/estatisticas.5b48807f.svg"
+  },
+  {
+    "revision": "35f798d01b74896299f27c50c3cd6816",
+    "url": "/reactjs/static/media/feed.35f798d0.svg"
+  },
+  {
+    "revision": "824d7d85ae3af9a43a61d46fe174192e",
+    "url": "/reactjs/static/media/login.824d7d85.jpg"
+  },
+  {
+    "revision": "4175ea7207f56280b48714a4133289cb",
+    "url": "/reactjs/static/media/sair.4175ea72.svg"
+  },
+  {
+    "revision": "dc4429925d170a392d67275b0e5f43b2",
+    "url": "/reactjs/static/media/usuario.dc442992.svg"
+  },
+  {
+    "revision": "957befc3d57033c55a88d144ae173685",
+    "url": "/reactjs/static/media/visualizacao-black.957befc3.svg"
+  },
+  {
+    "revision": "4ce15ed8e41974f50628587af3b32c4b",
+    "url": "/reactjs/static/media/visualizacao.4ce15ed8.svg"
+  }
+]);
